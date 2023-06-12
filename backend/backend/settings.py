78a7c6622b1f2dfab1159232ad0e3edb8df785ee
junho <anc5557@naver.com>
2023-06-12 16:12:14 +0900

    exp1 -> user 폴더명 변경

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'exp1',
+    'user',
     "rest_framework",
     'corsheaders',
 ]
--- backend/user/apps.py
+++ backend/user/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class Exp1Config(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "exp1"
+    name = "user"
