abf0c7b934494aa47016e375575804e7d6fc63fb
junho <anc5557@naver.com>
2023-06-12 10:50:08 +0900

    User 모델 추가

--- backend/exp1/models.py
+++ backend/exp1/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
+class User(models.Model):
+    
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=1)
+    birth = models.DateField()
+    position = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    email = models.EmailField(max_length=128)
+    
+    phoneNumberRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9]?)-?([0-9]{3,4})-?([0-9]{4})$')
+    phone = models.CharField(validators = [phoneNumberRegex], max_length = 11, unique = True)
+    
+    def __str__(self):
+        return self.name
+    
