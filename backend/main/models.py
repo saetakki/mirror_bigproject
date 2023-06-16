b9ef8d5f091f5aa1719adab902e97ff68b643c54
junho <anc5557@naver.com>
2023-06-16 17:07:07 +0900

    model 정의(유저, 히스토리, 페르소나)

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+# 유저
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    ID = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    profile_image = models.ImageField(upload_to='backend/imeges/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+
+# 페르소나
+class Persona(models.Model):
+    persona_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.persona_name
+    
+# 히스토리
+class history(models.Model):
+    # 유저와 히스토리는 1:N 관계
+    # 히스토리와 페르소나는 1:1 관계
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    bookmark = models.BooleanField(default=False)
+    chat_log = models.models.JSONField()
