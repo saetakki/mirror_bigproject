45cb2fd0881185fac7ec4423a4c153080b3de86e
JHY <yun01061991@naver.com>
2023-06-20 11:00:46 +0900

    add_sql

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 # uvicorn main:app --reload
 
 # Main imports
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, File, UploadFile, HTTPException, UploadFile
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from decouple import config
@@ -163,11 +163,25 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
+@app.post("/users/{user_id}/upload-image/")
+async def upload_image(user_id:int, file: UploadFile = File(...), db: Session = Depends(get_db)):
+    contents = await file.read()
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_user.file = contents
+    db.commit()
+    db.refresh(db_user)
+    return {"message": "Image uploaded successfully"}
+
 @app.patch("/users/{user_id}", response_model = schemas.User)
-async def update_user(user_id: int, user_update: schemas.UserUpdate, db: Session = Depends(get_db)):
+async def update_user(user_id: int, user_update: schemas.UserUpdate, file: UploadFile = File(...), db: Session = Depends(get_db)):
 	db_user = crud.get_user(db, user_id)
 	if db_user is None:
 		raise HTTPException(status_code=404, detail="User not found")
+	if file is not None:
+		file_data = await file.read()
+		user_update.file = file_data
 	updated_user = crud.update_user(db, user_id, user_update)
 	return updated_user
 
