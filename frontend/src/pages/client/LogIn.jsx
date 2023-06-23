078b4f34c2aa06821e08d611fd55ddc765883b3a
saetakki <lime.lemonade03@gmail.com>
2023-06-23 13:28:23 +0900

    feat: practice code

--- frontend/src/pages/client/LogIn.jsx
+++ frontend/src/pages/client/LogIn.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { requestLogin } from "@apis"
 import { useRecoilState } from "recoil";
 import { isAuthAtom } from "../../atoms";
 import { useNavigate } from "react-router-dom";
+import { Container } from "@styles";
 const LogIn = () => {
   const [, setIsAuth] = useRecoilState(isAuthAtom);
   const navigate = useNavigate();
+
+  const [isLogin, setIsLogin] = useState(true);
 
 
   const onClickHandler = (e) => {
@@ -18,8 +22,10 @@ const LogIn = () => {
   };
 
   return (
-    <button onClick={onClickHandler}>Log in</button>
-  );
+    <Container>
+      <button onClick={onClickHandler}>{isLogin ? "Log in" : "Sign up"}</button>
+      <div onClick={() => setIsLogin(!isLogin)}>{isLogin ? "회원가입으로" : "로그인으로"}</div>
+    </Container>)
 };
 
 export default LogIn;
