b4a26e0fbe71063e9ef10be24948328846dd3b31
JHY <yun01061991@naver.com>
2023-06-26 11:11:46 +0900

    sth changed

--- backend/chatapi/urls.py
+++ backend/chatapi/urls.py
@@ -2,7 +2,7 @@ from chatapi.views import set_persona, audio_to_text, get_text, get_ChatGPT_resp
 from django.urls import path
 
 urlpatterns = [
-    path('set_persona1/', set_persona),
+    path('set_persona/', set_persona),
     path('audio_to_text1/<int:history_id>/', audio_to_text),
     path('get_text1/<int:history_id>/', get_text),
     path('get_ChatGPT_response1/<int:history_id>/', get_ChatGPT_response),
