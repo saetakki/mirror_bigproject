3b5ed01c77d114f04b68ba113cc1c5441586f939
JHY <yun01061991@naver.com>
2023-06-26 11:40:53 +0900

    modify

--- backend/chatapi/urls.py
+++ backend/chatapi/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('set_persona/', set_persona),
-    path('audio_to_text1/<int:history_id>/', audio_to_text),
-    path('get_text1/<int:history_id>/', get_text),
-    path('get_ChatGPT_response1/<int:history_id>/', get_ChatGPT_response),
-    path('text_to_speech1/<int:history_id>/', text_to_speech),
-    path('make_report1/<int:history_id>/', make_report),
+    path('audio_to_text/<int:history_id>/', audio_to_text),
+    path('get_text/<int:history_id>/', get_text),
+    path('get_ChatGPT_response/<int:history_id>/', get_ChatGPT_response),
+    path('text_to_speech/<int:history_id>/', text_to_speech),
+    path('make_report/<int:history_id>/', make_report),
 ]	
