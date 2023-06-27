f72c37d888df89eb05cd8fab7256f267df2c0e00
saetakki <lime.lemonade03@gmail.com>
2023-06-27 15:30:14 +0900

    feat: Modifiy router

--- frontend/src/router/PrivateRoute.jsx
+++ frontend/src/router/PrivateRoute.jsx
@@ -1,18 +1,20 @@
 import { Navigate } from "react-router-dom";
-import { LogIn } from "@pages/client";
+import { LogIn, Test } from "@pages/client";
 import { Layout } from "@templates";
+import { useLocation } from "react-router-dom";
 
 export const PrivateRoute = ({withAuth, children}) =>{
 
   const isAuthenticated = window.localStorage.getItem("isAuth");
+  const isTest = useLocation().pathname;
 
   if(withAuth){
     return (isAuthenticated === null || isAuthenticated === "false") 
-    ? <Navigate to="/login" /> 
+    ? <Navigate to="/login"/>
     : <Layout>{children}</Layout>  } 
   else {
     return (isAuthenticated === null || isAuthenticated === "false") 
-    ? <LogIn/>
+    ? isTest === "/test" ? <Test/> : <LogIn/>
     : <Navigate to="/" replace />
   }
 }
