b164bf6d382628cd6eabd56f09c9566b7a43a99f
ARaOn <68447180+ARaOnn@users.noreply.github.com>
2023-06-30 18:22:18 +0900

    Update urls.py

--- backend/chatapi/urls.py
+++ backend/chatapi/urls.py
@@ -1,4 +1,4 @@
-from chatapi.views import set_persona, audio_to_text, get_text, get_ChatGPT_response, make_report, text_to_speech, make_sample_question
+from chatapi.views import set_persona, audio_to_text, get_text, get_ChatGPT_response, make_report,  make_sample_question
 from django.urls import path
 
 urlpatterns = [
@@ -6,7 +6,6 @@ urlpatterns = [
     path('audio_to_text/<int:history_id>/', audio_to_text),
     path('get_text/<int:history_id>/', get_text),
     path('get_ChatGPT_response/<int:history_id>/', get_ChatGPT_response),
-    path('text_to_speech/<int:history_id>/', text_to_speech),
     path('make_report/<int:history_id>/', make_report),
     path('make_sample_question/<int:history_id>/', make_sample_question)
 ]	
